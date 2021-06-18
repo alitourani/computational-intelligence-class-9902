a7c4918167b2bdc619eac0530212b14733ee827b
Farbod Nazari <81690217+farbodnm@users.noreply.github.com>
2021-06-18 22:24:52 +0430

    Update vocabulary
    Changed the return from set to dictionary.

--- G10-Semantic-Textual-Similarity/utils/vocabulary.py
+++ G10-Semantic-Textual-Similarity/utils/vocabulary.py
@@ -1,10 +1,15 @@
 def vocabulary(sentence_pairs):
-  vocab = set()
+  vocab_set = set()
   for i in range(len(sentence_pairs)):
     words = sentence_pairs[i][0].split()
     words += sentence_pairs[i][1].split()
     for j in range(len(words)):
-      vocab.add(words[j])
+      vocab_set.add(words[j])
+  vocab = {}
+  i = 0
+  for word in vocab_set:
+    vocab[word] = i
+    i += 1
 
   print('Made a vocabulary of', len(vocab), 'words.')
   return vocab
